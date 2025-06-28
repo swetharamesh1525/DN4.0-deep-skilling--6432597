9a4607bfe59974f7458e23c5357d1ac17ca9cf49
swetharamesh1525 <155316182+swetharamesh1525@users.noreply.github.com>
2025-06-29 04:09:43 +0530

    verifying interactions

---
+++ ExternalApi (1).java
@@ -0,0 +1,6 @@
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+interface ExternalApi {
+    void getData();
+}
---
+++ MyServiceTest (1).java
@@ -0,0 +1,10 @@
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
