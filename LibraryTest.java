e92a3dddf9f82b0b99f19a554da72075282fee78
swetharamesh1525 <155316182+swetharamesh1525@users.noreply.github.com>
2025-07-06 17:56:19 +0530

    Creating and configuring a maven project

---
+++ LibraryTest.java
@@ -0,0 +1,8 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryTest {
+    public static void main(String[] args) {
+        System.out.println("Spring Context Project Setup is Working!");
+    }
+}
+
