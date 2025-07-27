cc60c7948e0a006a3478a6bacdc488ce84015a61
swetharamesh1525 <155316182+swetharamesh1525@users.noreply.github.com>
2025-07-27 23:34:30 +0530

    studentapp

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
-import CohortDetails from './CohortDetails';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <CohortDetails name="React Bootcamp" status="ongoing" startDate="2025-06-01" endDate="2025-08-01" />
-      <CohortDetails name="Java Fundamentals" status="completed" startDate="2025-03-01" endDate="2025-05-30" />
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
